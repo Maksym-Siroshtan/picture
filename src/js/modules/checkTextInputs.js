cb21e5a5dbe463c14b7d2ad8375a0e9710d289fe
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-18 20:01:07 +0200

    Added form field validation module

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,19 @@
+const checkTextInputs = (selector) => {
+  const txtInputs = document.querySelectorAll(selector);
+
+  txtInputs.forEach((input) => {
+    input.addEventListener("keypress", function (e) {
+      if (e.key.match(/[^а-яё 0-9]/gi)) {
+        e.preventDefault();
+      }
+    });
+
+    input.addEventListener("input", () => {
+      if (input.value.match(/[a-z]/gi)) {
+        input.value = "";
+      }
+    });
+  });
+};
+
+export default checkTextInputs;
