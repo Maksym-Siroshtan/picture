010c1d73eabcee8203ecf403297e9a5d68d8e85a
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-19 20:18:27 +0200

    Added showMoreStyles module

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,24 @@
+const showMoreStyles = (triggerSelector, cardSelector) => {
+  const trigger = document.querySelector(triggerSelector),
+    cards = document.querySelectorAll(cardSelector);
+
+  cards.forEach((card) => {
+    card.classList.add("animated", "fadeInUp");
+  });
+
+  trigger.addEventListener("click", () => {
+    cards.forEach((card) => {
+      card.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs");
+      card.classList.add(
+        "col-sm-3",
+        "col-sm-offset-0",
+        "col-xs-10",
+        "col-xs-offset-1"
+      );
+    });
+
+    trigger.remove();
+  });
+};
+
+export default showMoreStyles;
