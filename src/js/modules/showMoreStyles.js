2251e72db879539854df87980062278521d8e706
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-20 18:53:25 +0200

    Added receiving cards from json-server

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,24 +1,52 @@
-const showMoreStyles = (triggerSelector, cardSelector) => {
-  const trigger = document.querySelector(triggerSelector),
-    cards = document.querySelectorAll(cardSelector);
+import { getResourse } from "../services/requests";
 
-  cards.forEach((card) => {
-    card.classList.add("animated", "fadeInUp");
+const showMoreStyles = (triggerSelector, wrapperSelector) => {
+  const trigger = document.querySelector(triggerSelector),
+    wrapper = document.querySelector(wrapperSelector);
+
+  trigger.addEventListener("click", function () {
+    getResourse("assets/db.json")
+      .then((res) => {
+        createCards(res.styles);
+      })
+      .catch((error) => {
+        let errorBlock = document.createElement("div");
+        errorBlock.classList.add("animated", "fadeInDown");
+
+        errorBlock.innerHTML = `
+          <h3 class="text-center text-danger">Извините, не возможно посмотреть 
+          больше стилей в данный момент... ${error}</h3>
+        `;
+        wrapper.appendChild(errorBlock);
+      });
+
+    this.remove();
   });
 
-  trigger.addEventListener("click", () => {
-    cards.forEach((card) => {
-      card.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs");
+  function createCards(response) {
+    response.forEach(({ src, title, link }) => {
+      let card = document.createElement("div");
+
       card.classList.add(
+        "animated",
+        "fadeInUp",
         "col-sm-3",
         "col-sm-offset-0",
         "col-xs-10",
         "col-xs-offset-1"
       );
-    });
 
-    trigger.remove();
-  });
+      card.innerHTML = `
+        <div class=styles-block>
+          <img src=${src} alt="Style">
+          <h4>${title}</h4>
+          <a href=${link}>Подробнее</a>
+        </div>
+      `;
+
+      wrapper.appendChild(card);
+    });
+  }
 };
 
 export default showMoreStyles;
