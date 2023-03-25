e0c9d9cf7f3773c3cfd45c0476e7b112a2157eb1
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-25 13:32:04 +0200

    Modified project.Added burger module

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from './modules/calc';
 import filter from './modules/filter';
 import picturesSize from './modules/pictureSize';
 import accordion from './modules/accordion';
+import burger from './modules/burger';
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -31,4 +32,5 @@ window.addEventListener("DOMContentLoaded", () => {
   filter();
   picturesSize('.sizes-block');
   accordion('.accordion-heading');
+  burger('.burger', '.burger-menu');
 });
