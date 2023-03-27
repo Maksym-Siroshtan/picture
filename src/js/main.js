da07c204de2fa67c4b425cbb20f7acbc2ac3e3fc
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-27 16:34:02 +0300

    Modified project.Added new functionality for smooth page scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import filter from './modules/filter';
 import picturesSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
+import scrolling from './modules/scrolling';
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -33,4 +34,5 @@ window.addEventListener("DOMContentLoaded", () => {
   picturesSize('.sizes-block');
   accordion('.accordion-heading');
   burger('.burger', '.burger-menu');
+  scrolling('.pageup');
 });
