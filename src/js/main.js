f20652f9060ea0c29080f21eeea49735faac8256
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-03-30 17:59:47 +0300

    Modified project.Added drop module

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import picturesSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
 import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -35,4 +36,5 @@ window.addEventListener("DOMContentLoaded", () => {
   accordion('.accordion-heading');
   burger('.burger', '.burger-menu');
   scrolling('.pageup');
+  drop();
 });
